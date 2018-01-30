51be8bf151364fda54bc9bc6dd949771ee2c66bc
Paul Pyschik <paulpyschik@gmail.com>
2018-01-30 10:06:57 +0100

    make the code easyer

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ $("aside a").click(function() {
 	$(this).parent().toggleClass("open");
 });
 
-
-$("a").click(function(){
-	$(this).removeClass(".section1")
-});
-
 $(".goto1").click(function(){
 	//It will remove all class active in tag a and section');
 	$("section, a").removeClass("active");
 	
 	//Now will add class "active" in THIS link');
-	$(this).addClass("active");
+	$(".goto1").addClass("active");
 
 	//Now, the section with the class section1 will recieve tha class active');
 	$(".section1").addClass("active");
@@ -25,7 +20,7 @@ $(".goto2").click(function(){
 	$("section, a").removeClass("active");
 	
 	//Now will add class "active" in THIS link');
-	$(this).addClass("active");
+	$(".goto2").addClass("active");
 	
 	//Now, the section with the class section2 will recieve tha class active');
 	$(".section2").addClass("active");
@@ -36,26 +31,8 @@ $(".goto3").click(function(){
 	$("section, a").removeClass("active");
 	
 	//Now will add class "active" in THIS link');
-	$(this).addClass("active");
+	$(".goto3").addClass("active");
 	
 	//Now, the section with the class section3 will recieve tha class active');
 	$(".section3").addClass("active");
-});
-
-$(".1").click(function() {
-	$("section, a").removeClass("active");
-	$(".section1").addClass("active");
-
-});
-
-$(".2").click(function() {
-	$("section, a").removeClass("active");
-	$(".section2").addClass("active");
-
-});
-
-$(".3").click(function() {
-	$("section, a").removeClass("active");
-	$(".section3").addClass("active");
-
 });
